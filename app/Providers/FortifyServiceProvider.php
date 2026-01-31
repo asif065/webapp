f23c31231bc96567ad9ca4052da7b3ca22628c0e
Asif Akter <shawon065@gmail.com>
2026-01-31 20:16:51 +0600

    redirect to login

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -66,7 +66,9 @@ class FortifyServiceProvider extends ServiceProvider
             'status' => $request->session()->get('status'),
         ]));
 
-        Fortify::registerView(fn () => Inertia::render('auth/login'));
+        //Fortify::registerView(fn () => Inertia::render('auth/register'));
+        Fortify::registerView(fn () => redirect()->route('login'));
+
 
         Fortify::twoFactorChallengeView(fn () => Inertia::render('auth/two-factor-challenge'));
 
