4e7b5bbebfdd6562b5dded3337bac3a7982fed36
Asif Akter <shawon065@gmail.com>
2026-01-31 16:23:01 +0600

    fix: trust proxy headers for https and correct asset urls

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * Trust all proxies (Ingress/LoadBalancers).
+     */
+    protected $proxies = '*';
+
+    /**
+     * Trust X-Forwarded-* headers (proto is critical for HTTPS).
+     */
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO;
+}
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,10 @@ return Application::configure(basePath: dirname(__DIR__))
             HandleInertiaRequests::class,
             AddLinkHeadersForPreloadedAssets::class,
         ]);
+
+        $middleware->use([
+            \App\Http\Middleware\TrustProxies::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
